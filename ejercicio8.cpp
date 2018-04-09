ed9881690221fb586124dba78c215142f3006135
Angela Aquise Santos <Angela18@users.noreply.github.com>
2018-04-09 12:32:58 -0500

    Update ejercicio8.cpp

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,30 +1,38 @@
 #include<iostream>
-#include<conio.h>
 #include<stdlib.h>
 
 using namespace std;
 
-void Intercambiar(int list1[],int list2[],int);
+void Intercambiar(char list1[],char list2[],int);
 
 int main(){
     int tam=5;
-    int list1[]={1,-2,3,-4,5};
-    int list2[]={-1,2,-3,4,-5};
-
+    char list1[tam]={'h','o','l','a'};
+    char list2[tam]={'m','a','m','a'};
+    cout<<"Lista 1: ";
+    for(int i=0;i<tam;i++){
+        cout<<list1[i];
+    }
+    cout<<endl;
+    cout<<"Lista 2: ";
+    for(int i=0;i<tam;i++){
+        cout<<list2[i];
+    }
+    cout<<endl;
     Intercambiar(list1,list2,tam);
     cout<<"La nueva lista 1 es: "<<endl;
     for(int i=0;i<tam;i++){
-        cout<<list2[i]<<" ";
+        cout<<list1[i];
     }
     cout<<endl;
     cout<<"La nueva lista 2 es: "<<endl;
     for(int i=0;i<tam;i++){
-        cout<<list1[i]<<" ";
+        cout<<list2[i];
     }
 
     return 0;
 }
-void Intercambiar(int list1[],int list2[],int tam){
+void Intercambiar(char list1[],char list2[],int tam){
     int aux;
     for(int i=0;i<tam;i++){
         aux=list1[i];
